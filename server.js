afb35bcbfeb0e8108f21704d56eb4ad35f58cb54
jgoldbeck <goldbeck.joseph@gmail.com>
2013-02-14 12:56:48 -0500

    Respond with JSON object inspection rather than JSON object itself

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ function start() {
 
     getcities.fromRequest(request, function(result) {
 
-          console.log("Inspect result:" + util.inspect(result, true, null));
+          //console.log("Inspect result:" + util.inspect(result, true, null));
 
           var url_parts = url.parse(request.url);
           var pathname = url_parts.pathname;
@@ -19,7 +19,7 @@ function start() {
 
           response.writeHead(200, {"Content-Type": "text/plain"});
           response.write("Hello World\n");
-          response.write("Result:" + result);
+          response.write("Result:" + util.inspect(result, true, null));
           response.end();
         });
   }
