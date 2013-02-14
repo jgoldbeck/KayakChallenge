254cf84b0bdf863f70c36e685ed890475f32e9e6
jgoldbeck <goldbeck.joseph@gmail.com>
2013-02-14 12:44:17 -0500

    Refactoring and allow for placename input in addition to zip code.

--- getcities.js
+++ getcities.js
@@ -3,33 +3,41 @@ var url = require("url");
 var querystring = require("querystring");
 var requestor = require('request');
 
+var fromRequest = function (request, callback){ //primary function which calls the various subfunctions in order
+    locationFromRequest(request, function(location) {
+      nearbyCitiesFromLocation(location, function(nearbycities){
+          callback(nearbycities)
+   //     });
+      });
+    });
+  }
 
-var zipFromRequest = function (request, callback){
+var locationFromRequest = function (request, callback){
  var zip_code = querystring.parse(url.parse(request.url).query)["zip"];
  callback(zip_code);
 }
 
-var jsonFromZip = function(zip_code, callback){
+var nearbyCitiesFromLocation = function(location, callback){
+  var radius = 30 * 1.609; // km
+  var maxrows = 100; // should be set to high number
   var options = {
-    url: 'http://ws.geonames.org/postalCodeLookupJSON?postalcode=02139&country=US&username=ms_test201302',
-    //json: true
+    url: 'http://ws.geonames.org/findNearbyPostalCodesJSON?placename=' + location +
+     '&radius=' + radius +
+     '&maxRows=' + maxrows +
+     '&style=short' +
+     '&username=ms_test201302',
+    json: true,
+    encoding: 'utf8'
   };
-
-  requestor.get(options, function (err, response, body) {
+  requestor.get(options, function (err, response, jsonbody) {
     if (err){
       console.log("Got error: " + err.message);
       callback(err.message);
     } else {
-      callback(body);
+      nearbyCities = jsonbody.postalCodes;
+      callback(nearbyCities);
     }
   });
 }
-var geoFromJSON = function(geoJSON){
 
-//   console.log('JSON: ' + geoJSON);
-}
-
-
-
-exports.zipFromRequest = zipFromRequest;
-exports.jsonFromZip = jsonFromZip;
+exports.fromRequest  = fromRequest;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var url = require("url");
+var util = require("util");
 var querystring = require("querystring");
 var getcities = require("./getcities");
 
@@ -8,19 +9,19 @@ var port = process.env.PORT || 5000;
 function start() {
   function onRequest(request, response) {
 
-    getcities.zipFromRequest(request, function(zipcode) {
-      getcities.jsonFromZip(zipcode, function(result){
+    getcities.fromRequest(request, function(result) {
 
-        var url_parts = url.parse(request.url);
-        var pathname = url_parts.pathname;
-        console.log("Request for " + pathname + " received.");
+          console.log("Inspect result:" + util.inspect(result, true, null));
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World\n");
-        response.write("Result:" + result);
-        response.end();
-      });
-    });
+          var url_parts = url.parse(request.url);
+          var pathname = url_parts.pathname;
+          console.log("Request for " + pathname + " received.");
+
+          response.writeHead(200, {"Content-Type": "text/plain"});
+          response.write("Hello World\n");
+          response.write("Result:" + result);
+          response.end();
+        });
   }
   http.createServer(onRequest).listen(port);
   console.log("Server has started.");
