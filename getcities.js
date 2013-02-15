483607f71714da818f37ffa0e0ae77ccb2337f33
jgoldbeck <goldbeck.joseph@gmail.com>
2013-02-15 00:06:49 -0500

    Halfway through adding weather to cities

--- getcities.js
+++ getcities.js
@@ -2,42 +2,98 @@ var http = require("http");
 var url = require("url");
 var querystring = require("querystring");
 var requestor = require('request');
+var _ = require('underscore');
+var async = require('async');
 
-var fromRequest = function (request, callback){ //primary function which calls the various subfunctions in order
-    locationFromRequest(request, function(location) {
-      nearbyCitiesFromLocation(location, function(nearbycities){
-          callback(nearbycities)
-   //     });
-      });
+var geonames_user = 'ms_test201302';
+var wxbug_api_key = 'fbzpn6ambx9de5nusf49rv4d';
+
+var fromRequest = function (request, callback) { //primary function which calls the various subfunctions in order
+    var location = locationFromRequest(request);
+    var location_options = {
+        location: location
+    };
+    nearbyCitiesFromLocation(location_options, function(nearby_cities) {
+    attachWeatherToCities(nearby_cities, function(weathered_cities) {
+    callback(weathered_cities);}
+    );
+
+});
+};
+
+var locationFromRequest = function (request) {
+    return querystring.parse(url.parse(request.url).query)["dest"];
+};
+
+var nearbyCitiesFromLocation = function(location_options, callback){
+    _.defaults(location_options, {
+        location: '02139',
+        radius: 30,
+        maxrows: 100
     });
-  }
+location_options.radius = Math.ceil(location_options.radius * 1.609); // convert from mi to km
 
-var locationFromRequest = function (request, callback){
- var zip_code = querystring.parse(url.parse(request.url).query)["dest"];
- callback(zip_code);
-}
-
-var nearbyCitiesFromLocation = function(location, callback){
-  var radius = 30 * 1.609; // km
-  var maxrows = 100; // should be set to high number
-  var options = {
-    url: 'http://ws.geonames.org/findNearbyPostalCodesJSON?placename=' + location +
-     '&radius=' + radius +
-     '&maxRows=' + maxrows +
-     '&style=short' +
-     '&username=ms_test201302',
+var geo_options = {
+    url: 'http://ws.geonames.org/findNearbyPostalCodesJSON?placename=' + location_options.location +
+    '&radius=' + location_options.radius +
+    '&maxRows=' + location_options.maxrows +
+    '&style=short' +
+    '&username=' + geonames_user,
     json: true,
     encoding: 'utf8'
-  };
-  requestor.get(options, function (err, response, jsonbody) {
+};
+
+//get postal codes
+requestor.get(geo_options, function (err, response, jsonbody) {
     if (err){
-      console.log("Got error: " + err.message);
-      callback(err.message);
+        console.log("Got error: " + err.message);
+        callback(err.message);
     } else {
-      nearbyCities = jsonbody.postalCodes;
-      callback(nearbyCities);
+        nearbyCities = jsonbody.postalCodes;
+        callback(nearbyCities);
     }
+});
+};
+
+var attachWeatherToCities = function(cities, callback) {
+weathered_cities = cities.reverse();
+    async.each(cities.reverse(), function(city,callback){  // reverse cities so that duplicate city names will be overwritten by zip codes
+        //                                                                               // it would be boring if user was directed to several zip codes within literally the same city
+        callback();
+    },
+    function(err){
+        if (err) {
+            console.log("Got error: " + err.message);
+            callback(err.message);}
+        else {
+            callback(weathered_cities);
+        }
+    });
+
+} ;
+
+var attachWeatherToCity = function (city, callback) {
+
+    zip_code=(city.postalCode);
+    var wxbug_options = {
+        url: 'http://i.wxbug.net/REST/Direct/GetForecast.ashx?zip=' + zip_code +
+        '&nf=7&ht=t&l=en&c=US&api_key=' + wxbug_api_key,
+        json: true
+    };
+    requestor.get(wxbug_options, function (err, response, jsonweather) {
+        if (err){
+            console.log("Got error: " + err.message);
+            callback(err.message);
+        } else {
+          city.weather = jsonweather.forecastList;
+          callback(city);
+      }
+
   });
-}
+};
+
+
+
+
 
 exports.fromRequest  = fromRequest;
