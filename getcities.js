4925f4347a1551aad04ae8e80089a2b76b29de01
jgoldbeck <goldbeck.joseph@gmail.com>
2013-02-15 02:18:54 -0500

    async to wxbug. requests have a wait. still need to check wxbug return.

--- getcities.js
+++ getcities.js
@@ -26,14 +26,14 @@ var locationFromRequest = function (request) {
 };
 
 var nearbyCitiesFromLocation = function(location_options, callback){
-    _.defaults(location_options, {
+    _.defaults(location_options, { // set defaults
         location: '02139',
         radius: 30,
         maxrows: 100
     });
 location_options.radius = Math.ceil(location_options.radius * 1.609); // convert from mi to km
 
-var geo_options = {
+var geo_options = { //options for calling geonames service
     url: 'http://ws.geonames.org/findNearbyPostalCodesJSON?placename=' + location_options.location +
     '&radius=' + location_options.radius +
     '&maxRows=' + location_options.maxrows +
@@ -56,11 +56,20 @@ requestor.get(geo_options, function (err, response, jsonbody) {
 };
 
 var attachWeatherToCities = function(cities, callback) {
-    var weathered_cities = cities;
+    var weathered_cities = {};
     cities.reverse(); // reverse cities so that duplicate city names will be overwritten by zip codes. it would be boring if user was directed to several zip codes within literally the same city
+    var wait = 0;
     async.each(cities, function(city,callback){
-        //
-        callback();
+        wait += 500;  // need to delay because of api rate limit
+        console.log(wait);
+        var thing = function(weathered_city){
+
+            //console.log(weathered_cities);
+            callback(); // weathered_cities[city.location] = weathered_city;
+
+        };
+        setTimeout(function() {attachWeatherToCity(city, thing);}, wait);
+
     },
     function(err){
         if (err) {
@@ -76,17 +85,18 @@ var attachWeatherToCities = function(cities, callback) {
 var attachWeatherToCity = function (city, callback) {
 
     zip_code=(city.postalCode);
-    var wxbug_options = {
+    var wxbug_options = { // options for calling weatherbug service
         url: 'http://i.wxbug.net/REST/Direct/GetForecast.ashx?zip=' + zip_code +
         '&nf=7&ht=t&l=en&c=US&api_key=' + wxbug_api_key,
         json: true
     };
-    requestor.get(wxbug_options, function (err, response, jsonweather) {
+    requestor.get(wxbug_options, function (err, response, jsonweather) { //attach weather to the city object
         if (err){
             console.log("Got error: " + err.message);
-            callback(err.message);
+            callback(err.message); // check is response is 200?????
         } else {
           city.weather = jsonweather.forecastList;
+          console.log(city);
           callback(city);
       }
 
